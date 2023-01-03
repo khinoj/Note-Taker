3620ff18c332e13d58459b1f175d0b8cd8898ef5
khinoj <kristopher.hinojosa@gmail.com>
2023-01-02 20:34:09 -0500

    moved class example into file

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,24 +1,47 @@
-//Get Express
+// //Get Express
+// const express = require('express');
+// const apiRoutes = require('../routes/apiRoutes');
+// const htmlRoutes = require('../routes/htmlRoutes');
+
+// const app = express();
+
+// // Listen to this Port
+// const PORT = process.env.PORT || 4000;  //maybe need 3000?
+
+// //Setup Express
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.static('public'));
+// app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes)
+
+
+
+// app.listen(PORT, function () {
+//     console.log('Server Lisening on http://localhost' + PORT);
+// })
+
+/// From Class
+
 const express = require('express');
-const apiRoutes = require('../routes/apiRoutes');
-const htmlRoutes = require('../routes/htmlRoutes');
+const path = require('path');
 
 const app = express();
+const PORT = 3001;
 
-// Listen to this Port
-const PORT = process.env.PORT || 4000;  //maybe need 3000?
-
-//Setup Express
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes)
 
+app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
 
+app.get('/send', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/send.html'))
+);
 
-app.listen(PORT, function () {
-    console.log('Server Lisening on http://localhost' + PORT);
-})
+app.get('/paths', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/paths.html'))
+);
 
+app.listen(PORT, () =>
+    console.log(`Example app listening at http://localhost:${PORT}`)
+);
 
