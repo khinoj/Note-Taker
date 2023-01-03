8d6f292ff24ab5c1e1e72fb179b57735f671851a
khinoj <kristopher.hinojosa@gmail.com>
2023-01-02 20:09:37 -0500

    server js updates with file making

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,10 +1,24 @@
 //Get Express
 const express = require('express');
+const apiRoutes = require('../routes/apiRoutes');
+const htmlRoutes = require('../routes/htmlRoutes');
 
 const app = express();
 
 // Listen to this Port
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000;  //maybe need 3000?
 
+//Setup Express
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes)
+
+
+
+app.listen(PORT, function () {
+    console.log('Server Lisening on http://localhost' + PORT);
+})
 
 
