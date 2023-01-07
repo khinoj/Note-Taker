711875cb2100638fc0db5469c2e3db521bce98fd
khinoj <kristopher.hinojosa@gmail.com>
2023-01-07 16:15:53 -0500

    idk heroku garbage3

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,7 @@
 const notes = require('express').Router();
+const { readFromFile, readAndAppend, writeToFile } = require('../helpers/fsUtils');
 
-// GET Route for notes
+// GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
@@ -18,7 +19,24 @@ notes.get('/:id', (req, res) => {
     });
 });
 
-// POST Route 
+// DELETE Route for a specific note
+notes.delete('/:id', (req, res) => {
+  const noteId = req.params.id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all notes except the one with the ID provided in the URL
+      const result = json.filter((note) => note.id !== noteId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/db.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${noteId} has been deleted 🗑️`);
+    });
+});
+
+// POST Route for a new note
 notes.post('/', (req, res) => {
   console.log(req.body);
 
@@ -32,9 +50,9 @@ notes.post('/', (req, res) => {
     };
 
     readAndAppend(newNote, './db/db.json');
-    res.json(`Note Added`);
+    res.json(`Note Added!`);
   } else {
-    res.error('Error Didnt Add Note');
+    res.error('Error Adding Note');
   }
 });
 
